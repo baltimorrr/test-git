873007f88993877465bc8b84cc520474e120f48f
jazzy <minh15553@gmail.com>
2022-05-27 10:24:19 +0700

    promise

--- hello.js
+++ hello.js
@@ -1,1 +1,7 @@
-console.log("minhabcd")
+let promise = new Promise(function (resolve, reject) {
+    setTimeout(() => resolve("done"), 1000)
+})
+
+promise.then(
+    (res) => console.log(res)
+).catch((err) => console.log(err))
